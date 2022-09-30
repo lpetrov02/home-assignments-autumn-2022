0c97d96e32ba2795387b4ef96e4d67644934ad6a
lpetrov02 <lpetrov02@mail.ru>
2022-10-01 00:58:07 +0300

    Уголки не мигают, треки отслеживаются

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -75,14 +75,19 @@ def draw_corners(img, corners, radius=3, color=(0, 1, 0),
     return rgb
 
 
+def fill_around_zero(mask, i, j, r=10):
+    mask[max(0, i - r):min(mask.shape[0], i + r), max(0, j - r):min(mask.shape[1], j + r)] = 0
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
     opt_flow_params = dict(
         winSize=(25, 25), maxLevel=5, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
     )
 
+    n_corners = 500
     image_0 = np.uint8(frame_sequence[0] * 255)
-    corners_coordinates = cv2.goodFeaturesToTrack(image_0, 200, 0.01, 10).reshape(-1, 2)
+    corners_coordinates = cv2.goodFeaturesToTrack(image_0, n_corners, 0.005, 10).reshape(-1, 2)
     corners_ids = np.arange(corners_coordinates.shape[0])
     next_id = corners_coordinates.shape[0]
     corners = FrameCorners(
@@ -97,27 +102,33 @@ def _build_impl(frame_sequence: pims.FramesSequence,
         old_corners, status, err = cv2.calcOpticalFlowPyrLK(
             image_0, image_1, corners_coordinates, None, **opt_flow_params
         )
-        old_corners = old_corners.reshape((-1, 2)).astype(int)
-        mask = np.array([False] * image_1.shape[0] * image_1.shape[1]).reshape(image_1.shape)
+        old_corners = old_corners.reshape((-1, 2))
+        mask_param = np.ones(image_1.shape).astype(int) * 255
         for i, cor in enumerate(old_corners):
             if status[i, 0]:
                 new_ids.append(corners_ids[i])
                 new_corners.append(cor)
-                mask[cor[1], cor[0]] = True
+                fill_around_zero(mask_param, int(cor[1]), int(cor[0]), r=10)
         new_ids = np.array(new_ids)
         new_corners = np.array(new_corners)
 
-        new_corners_2 = cv2.goodFeaturesToTrack(image_1, 200, 0.01, 10, mask=mask, blockSize=(10, 10)).reshape(-1, 2)
-        new_ids_2 = np.arange(next_id, next_id + new_corners_2.shape[0])
-        next_id += new_corners_2.shape[0]
+        if new_corners.shape[0] < n_corners:
+            new_corners_2 = cv2.goodFeaturesToTrack(image_1, max(1, n_corners - new_corners.shape[0]), 0.01, 10,
+                                                    mask=np.uint8(mask_param), blockSize=10).reshape(-1, 2)
+            new_ids_2 = np.arange(next_id, next_id + new_corners_2.shape[0])
+            next_id += new_corners_2.shape[0]
+            corners_coordinates = np.vstack([new_corners, new_corners_2])
+            corners_ids = np.hstack([new_ids, new_ids_2])
+        else:
+            corners_coordinates = new_corners
+            corners_ids = new_ids
 
-        corners_coordinates = np.vstack([new_corners, new_corners_2])
-        corners_ids = np.hstack([new_ids, new_ids_2])
         corners = FrameCorners(
             corners_ids,
             corners_coordinates,
             np.ones(corners_coordinates.shape[0]) * 10
         )
+
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
