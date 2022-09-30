ab117c3feb3d60941518a2d4990ccd3bd0485e1b
lpetrov02 <lpetrov02@mail.ru>
2022-09-30 23:58:26 +0300

    note done yet, bur coming very soon!

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -16,6 +16,8 @@ import click
 import cv2
 import numpy as np
 import pims
+import matplotlib.pyplot as plt
+
 
 from _corners import (
     FrameCorners,
@@ -46,17 +48,76 @@ class _CornerStorageBuilder:
         return StorageImpl(item[1] for item in sorted(self._corners.items()))
 
 
+class Corner:
+    def __init__(self, i, j, c_id=0):
+        self._i = i
+        self._j = j
+        self._id = c_id
+
+    def __hash__(self):
+        return hash(f"{self._i},{self._j}")
+
+
+def draw_corners(img, corners, radius=3, color=(0, 1, 0),
+                 y_first=True):
+    if len(img.shape) == 2:
+        rgb = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    elif len(img.shape) == 3:
+        rgb = img.copy()
+    else:
+        raise ValueError('Illegal image format')
+    for pt in corners:
+        if y_first:
+            pt_tuple = tuple(pt[::-1])
+        else:
+            pt_tuple = tuple(pt)
+        cv2.circle(rgb, pt_tuple, radius, color)
+    return rgb
+
+
 def _build_impl(frame_sequence: pims.FramesSequence,
                 builder: _CornerStorageBuilder) -> None:
-    # TODO
-    image_0 = frame_sequence[0]
+    opt_flow_params = dict(
+        winSize=(25, 25), maxLevel=5, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03)
+    )
+
+    image_0 = np.uint8(frame_sequence[0] * 255)
+    corners_coordinates = cv2.goodFeaturesToTrack(image_0, 200, 0.01, 10).reshape(-1, 2)
+    corners_ids = np.arange(corners_coordinates.shape[0])
+    next_id = corners_coordinates.shape[0]
     corners = FrameCorners(
-        np.array([0]),
-        np.array([[0, 0]]),
-        np.array([55])
+        corners_ids,
+        corners_coordinates,
+        np.ones(corners_coordinates.shape[0]) * 15
     )
     builder.set_corners_at_frame(0, corners)
     for frame, image_1 in enumerate(frame_sequence[1:], 1):
+        image_1 = np.uint8(image_1 * 255)
+        new_corners, new_ids = [], []
+        old_corners, status, err = cv2.calcOpticalFlowPyrLK(
+            image_0, image_1, corners_coordinates, None, **opt_flow_params
+        )
+        old_corners = old_corners.reshape((-1, 2)).astype(int)
+        mask = np.array([False] * image_1.shape[0] * image_1.shape[1]).reshape(image_1.shape)
+        for i, cor in enumerate(old_corners):
+            if status[i, 0]:
+                new_ids.append(corners_ids[i])
+                new_corners.append(cor)
+                mask[cor[1], cor[0]] = True
+        new_ids = np.array(new_ids)
+        new_corners = np.array(new_corners)
+
+        new_corners_2 = cv2.goodFeaturesToTrack(image_1, 200, 0.01, 10, mask=mask, blockSize=(10, 10)).reshape(-1, 2)
+        new_ids_2 = np.arange(next_id, next_id + new_corners_2.shape[0])
+        next_id += new_corners_2.shape[0]
+
+        corners_coordinates = np.vstack([new_corners, new_corners_2])
+        corners_ids = np.hstack([new_ids, new_ids_2])
+        corners = FrameCorners(
+            corners_ids,
+            corners_coordinates,
+            np.ones(corners_coordinates.shape[0]) * 10
+        )
         builder.set_corners_at_frame(frame, corners)
         image_0 = image_1
 
