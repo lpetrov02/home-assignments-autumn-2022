aa2767fc9773a712c00130cb0350ebae73bba90f
lpetrov02 <lpetrov02@mail.ru>
2022-12-19 12:05:13 +0300

    Adds angle check

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -34,7 +34,7 @@ def solve_pnp(intrinsic_matrix, frame_number, calculated_points, calculated_poin
     image_points = dict(zip(frame_corners.ids.reshape(-1), frame_corners.points))
     calc_points = dict(zip(calculated_points_ids.reshape(-1), calculated_points))
     common_ids_set = set(calculated_points_ids.reshape(-1)) & set(frame_corners.ids.reshape(-1)) - excluded
-    if len(common_ids_set) < 4:
+    if len(common_ids_set) < 6:
         return None, None
 
     common_ids = list(common_ids_set)
@@ -45,6 +45,9 @@ def solve_pnp(intrinsic_matrix, frame_number, calculated_points, calculated_poin
         actual_image_points.append(image_points[corner_id])
     retval, rvec, tvec, inliers = cv2.solvePnPRansac(np.array(actual_points), np.array(actual_image_points),
                                                      intrinsic_matrix, np.zeros(4))
+    if retval and len(inliers) >= 6:
+        retval, rvec, tvec = cv2.solvePnP(np.array(actual_points)[inliers], np.array(actual_image_points)[inliers],
+                                                         intrinsic_matrix, np.zeros(4))
     view_mat = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
     inliers_ids = np.array(common_ids)[inliers]
     excluded |= (common_ids_set - set(inliers_ids.reshape(-1).tolist()))
@@ -83,7 +86,7 @@ def triangulate_n_points(points2d, camera_poses, proj_mat):
     for i in range(len(points[0])):
         A = np.vstack([[P[j][3, :] * points[j, i, 0] - P[j][0, :], P[j][3, :] * points[j, i, 1] - P[j][1, :]]
                        for j in range(len(points))])
-        X = np.linalg.lstsq(A[:, :3] / A[:, 3].reshape(-1, 1), -np.ones(A.shape[0]), rcond=0)
+        X = np.linalg.lstsq(A[:, :3] / A[:, 3].reshape(-1, 1), -np.ones(A.shape[0], 1), rcond=0)
         res_points.append(X[0])
     return np.array(res_points)
 
@@ -95,7 +98,7 @@ def initialize_prim(corners_1, corners_2, K):
     points_1 = corners_1.points[indices_1].reshape(-1, 2)
     points_2 = corners_2.points[indices_2].reshape(-1, 2)
 
-    if points_1.shape[0] < 5:
+    if points_1.shape[0] < 6:
         return False, None, None, None, None, None
 
     E_base, mask_base = cv2.findEssentialMat(points_1, points_2, K, method=cv2.RANSAC)
@@ -105,63 +108,103 @@ def initialize_prim(corners_1, corners_2, K):
         retval, R, t, mask, _ = cv2.recoverPose(
             E_base, points1=points_1, points2=points_2, cameraMatrix=K, distanceThresh=50
         )
-        return True, R, t, mask_base, mask_base.mean() - mask_homo.mean(), mask_base.sum()
+        return True, R, t, mask_base, mask_base.mean(), mask_base.sum()
     return False, None, None, None, None, None
 
 
-def initialize_short(intrinsic_mat, corner_storage, min_inliers_count=4, good_inliers_count=500):
+def initialize_short(intrinsic_mat, corner_storage, min_inliers_count=6):
     best_res = {"R": np.eye(3), "t": np.zeros(3), "n": (0, 1)}
-    best_inliers_count = 0
+    success = False
+
+    best_inliers_count, good_inliers_count = 0, 500
+    best_inliers_part, good_inliers_part = 0.0, 0.8
+    min_angle = 30
 
     fc = len(corner_storage)
     centers = [fc // 2]
 
-    for center in centers:
-        f1 = center // 2 - min(fc // 4, 10)
-        f2 = center // 2 + min(fc // 4, 10)
+    while True:
+        for center in centers:
+            f1 = center // 2 - min(fc // 4, 10)
+            f2 = center // 2 + min(fc // 4, 10)
 
-        while f2 - f1 > 0:
-            res, R, t, mask, inliers_part, inliers_count =\
-                initialize_prim(corner_storage[f1], corner_storage[f2], intrinsic_mat)
-            print(f1, f2, inliers_count)
-            if res and inliers_count >= min_inliers_count:
-                if inliers_count > best_inliers_count:
-                    best_res["R"], best_res["t"] = R, t.flatten()
-                    best_res["n"] = (f1, f2)
-                    best_inliers_count = inliers_count
-                if inliers_count > good_inliers_count:
-                    break
-            if abs(center - f1) > abs(center - f2):
-                f1 += 1
-            else:
-                f2 -= 1
-            continue
+            while f2 - f1 > 0:
+                res, R, t, mask, inliers_part, inliers_count =\
+                    initialize_prim(corner_storage[f1], corner_storage[f2], intrinsic_mat)
+
+                correspondences = build_correspondences(corner_storage[f1], corner_storage[f2])
+                points3d, correspondences_ids, median_cos = \
+                    triangulate_correspondences(
+                        correspondences,
+                        pose_to_view_mat3x4(Pose(np.eye(3), -np.zeros(3))),
+                        pose_to_view_mat3x4(Pose(np.linalg.inv(R), -R @ t)),
+                        intrinsic_mat,
+                        TriangulationParameters(50, 0, 0)
+                    )
+                if median_cos < np.cos(min_angle / 180 * np.pi):
+                    if res and inliers_count >= min_inliers_count:
+                        if inliers_part > best_inliers_part:
+                            best_res["R"], best_res["t"] = R, t.flatten()
+                            best_res["n"] = (f1, f2)
+                            best_inliers_part = inliers_part
+                            success = True
+                            if inliers_count > good_inliers_count:
+                                break
+                if abs(center - f1) > abs(center - f2):
+                    f1 += 1
+                else:
+                    f2 -= 1
+        if success or min_angle == 0:
+            break
+        else:
+            min_angle -= 5
     return best_res
 
 
-def initialize_long(intrinsic_mat, corner_storage, min_inliers_count=4, good_inliers_count=500):
+def initialize_long(intrinsic_mat, corner_storage, min_inliers_count=6, good_inliers_count=500):
     best_res = {"R": np.eye(3), "t": np.zeros(3), "n": (0, 1)}
-    best_inliers_count = 0
+    success = False
+
+    best_inliers_count, good_inliers_count = 0, 500
+    best_inliers_part, good_inliers_part = 0.0, 0.8
+    min_angle = 30
 
     fc = len(corner_storage)
-    d = fc // 4
-    while d > 0:
-        f1, f2 = 0, d - 1
-        while f2 < fc:
-            res, R, t, mask, inliers_part, inliers_count =\
-                initialize_prim(corner_storage[f1], corner_storage[f2], intrinsic_mat)
-            if not res:
+    while True:
+        d = fc // 4
+        while d > 0:
+            f1, f2 = 0, d - 1
+            while f2 < fc:
+                res, R, t, mask, inliers_part, inliers_count =\
+                    initialize_prim(corner_storage[f1], corner_storage[f2], intrinsic_mat)
+                if not res:
+                    f1, f2 = f1 + d // 2, f2 + d // 2
+                    continue
+
+                correspondences = build_correspondences(corner_storage[f1], corner_storage[f2])
+                points3d, correspondences_ids, median_cos = \
+                    triangulate_correspondences(
+                        correspondences,
+                        pose_to_view_mat3x4(Pose(np.eye(3), -np.zeros(3))),
+                        pose_to_view_mat3x4(Pose(np.linalg.inv(R), -R @ t)),
+                        intrinsic_mat,
+                        TriangulationParameters(50, 0, 0)
+                    )
+                if median_cos >= np.cos(min_angle / 180 * np.pi):
+                    if inliers_count >= min_inliers_count:
+                        if inliers_part > best_inliers_part:
+                            best_res["R"], best_res["t"] = R, t.flatten()
+                            best_res["n"] = (f1, f2)
+                            best_inliers_part = inliers_part
+                            success = True
+                        if inliers_count > good_inliers_count:
+                            return best_res
                 f1, f2 = f1 + d // 2, f2 + d // 2
-                continue
-            if inliers_count >= min_inliers_count:
-                if inliers_count > best_inliers_count:
-                    best_res["R"], best_res["t"] = R, t.flatten()
-                    best_res["n"] = (f1, f2)
-                    best_inliers_count = inliers_count
-            if inliers_count > good_inliers_count:
-                return best_res
-            f1, f2 = f1 + d // 2, f2 + d // 2
-        d = d * 9 // 10
+            d = d * 4 // 5
+        if success or min_angle == 0:
+            break
+        else:
+            min_angle -= 5
     return best_res
 
 
@@ -171,9 +214,7 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None) \
         -> Tuple[List[Pose], PointCloud]:
-    if known_view_1 is None or known_view_2 is None:
-        raise NotImplementedError()
-    if known_view_1[0] > known_view_2[0]:
+    if known_view_1 is not None and known_view_2 is not None and known_view_1[0] > known_view_2[0]:
         known_view_1, known_view_2 = known_view_2, known_view_1
 
     rgb_sequence = frameseq.read_rgb_f32(frame_sequence_path)
@@ -188,49 +229,54 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
 
     frame_count = len(corner_storage)
     view_mats = [None] * frame_count
-    # view_mats[known_view_1[0]] = pose_to_view_mat3x4(known_view_1[1])
-    # view_mats[known_view_2[0]] = pose_to_view_mat3x4(known_view_2[1])
-    # correspondences = build_correspondences(corner_storage[known_view_1[0]], corner_storage[known_view_2[0]])
-    # points3d, correspondences_ids, median_cos =\
-    #     triangulate_correspondences(
-    #         correspondences,
-    #         pose_to_view_mat3x4(known_view_1[1]),
-    #         pose_to_view_mat3x4(known_view_2[1]),
-    #         intrinsic_mat,
-    #         TriangulationParameters(50, 0, 0)
-    #     )
-    #
-    # point_cloud_builder = PointCloudBuilder(correspondences_ids, points3d)
-    # print(f"First two frames processed: {known_view_1[0]} and {known_view_2[0]}")
-    # print(f"3D points on the 1st step: {len(points3d)}, point cloud size: {len(points3d)}")
 
-    if frame_count >= 400:
-        view = initialize_long(intrinsic_mat, corner_storage)
+    if known_view_1 is not None and known_view_2 is not None:
+        view_mats[known_view_1[0]] = pose_to_view_mat3x4(known_view_1[1])
+        view_mats[known_view_2[0]] = pose_to_view_mat3x4(known_view_2[1])
+        correspondences = build_correspondences(corner_storage[known_view_1[0]], corner_storage[known_view_2[0]])
+        points3d, correspondences_ids, median_cos =\
+            triangulate_correspondences(
+                correspondences,
+                pose_to_view_mat3x4(known_view_1[1]),
+                pose_to_view_mat3x4(known_view_2[1]),
+                intrinsic_mat,
+                TriangulationParameters(50, 0, 0)
+            )
+
+        point_cloud_builder = PointCloudBuilder(correspondences_ids, points3d)
+        print(f"First two frames processed: {known_view_1[0]} and {known_view_2[0]}")
+        print(f"3D points on the 1st step: {len(points3d)}, point cloud size: {len(points3d)}")
+        processed_frames_set = {known_view_1[0], known_view_2[0]}
     else:
-        view = initialize_short(intrinsic_mat, corner_storage)
-    view_mats[view["n"][0]] = pose_to_view_mat3x4(Pose(np.eye(3), -np.zeros(3)))
-    view_mats[view["n"][1]] = pose_to_view_mat3x4(Pose(np.linalg.inv(view["R"]), -view["R"] @ view["t"]))
-    correspondences = build_correspondences(corner_storage[view["n"][0]], corner_storage[view["n"][1]])
-    points3d, correspondences_ids, median_cos =\
-        triangulate_correspondences(
-            correspondences,
-            view_mats[view["n"][0]],  # pose_to_view_mat3x4(Pose(np.eye(3), -np.zeros(3))),
-            view_mats[view["n"][1]],  # pose_to_view_mat3x4(Pose(view["R"], -view["R"] @ view["t"])),
-            intrinsic_mat,
-            TriangulationParameters(50, 0, 0)
-        )
+        if frame_count >= 400:
+            print("'Long' init case")
+            view = initialize_long(intrinsic_mat, corner_storage)
+        else:
+            print("'Short' init case")
+            view = initialize_short(intrinsic_mat, corner_storage)
+        view_mats[view["n"][0]] = pose_to_view_mat3x4(Pose(np.eye(3), -np.zeros(3)))
+        view_mats[view["n"][1]] = pose_to_view_mat3x4(Pose(np.linalg.inv(view["R"]), -view["R"] @ view["t"]))
+        correspondences = build_correspondences(corner_storage[view["n"][0]], corner_storage[view["n"][1]])
+        points3d, correspondences_ids, median_cos =\
+            triangulate_correspondences(
+                correspondences,
+                view_mats[view["n"][0]],
+                view_mats[view["n"][1]],
+                intrinsic_mat,
+                TriangulationParameters(50, 0, 0)
+            )
 
-    point_cloud_builder = PointCloudBuilder(correspondences_ids, points3d)
-    print(f"First two frames processed: {view['n'][0]} and {view['n'][1]}")
-    print(f"3D points on the 1st step: {len(points3d)}, point cloud size: {len(points3d)}")
-
-    processed_frames_set = {view["n"][0], view["n"][1]}
+        point_cloud_builder = PointCloudBuilder(correspondences_ids, points3d)
+        print(f"corner storage size: {len(corner_storage)}")
+        print(f"First two frames processed (init): {view['n'][0]} and {view['n'][1]}")
+        print(f"3D points on the 1st step: {len(points3d)}, point cloud size: {len(points3d)}")
+        processed_frames_set = {view["n"][0], view["n"][1]}
 
     # NOW WE HAVE TO PROCESS ALL FRAMES OF THE VIDEO
     shift = frame_count
-    min_angle = 10
+    min_angle = 20
     added_points_on_previous_epoch = 2
-    min_common_points = 2
+    min_common_points = 6
     direction_parameter = {"frw": {"dir": 1, "end": frame_count}, "back": {"dir": -1, "end": -1}}
     outliers = set()
     steps = 0
@@ -245,16 +291,16 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     new_correspondences_n = 0
                     left_shift, right_shift = fn, frame_count - fn - 1
                     new_correspondences_ids, new_correspondences_points = None, None
-                    while new_correspondences_n < 5 and left_shift * right_shift > 0:
+                    while new_correspondences_n < 6 and left_shift * right_shift > 0:
                         new_correspondences_ids, new_correspondences_points = build_extended_correspondences(
                             [corner_storage[fn - left_shift],
                              corner_storage[fn],
                              corner_storage[fn + right_shift]]
                         )
                         new_correspondences_n = len(new_correspondences_ids)
-                        if new_correspondences_n < 5:
-                            left_shift, right_shift = left_shift * 9 // 10, right_shift * 9 // 10
-                    if new_correspondences_n >= 5:
+                        if new_correspondences_n < 6:
+                            left_shift, right_shift = left_shift * 4 // 5, right_shift * 4 // 5
+                    if new_correspondences_n >= 6:
                         new_points3d = triangulate_n_points(new_correspondences_points,
                                                             [
                                                                 view_mats[fn],
@@ -312,7 +358,7 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     new_fn += 1 * direction_parameter[d]["dir"]
         if len(processed_frames_set) == added_points_on_previous_epoch:
             shift = shift * 3 // 4
-            min_angle //= 2
+            min_angle = min_angle * 3 // 4
         added_points_on_previous_epoch = len(processed_frames_set)
 
     calc_point_cloud_colors(
